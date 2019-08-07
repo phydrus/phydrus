5923ce236785bd941fee813b17fdca94fb386c58
Raoul Collenteur <raoulcollenteur@gmail.com>
2019-08-07 08:43:12 +0200

    Add example 2 and 3 with transient water flow

---
+++ examples/example2.py
@@ -0,0 +1,47 @@
+"""
+Example script of how to setup a basic HYDRUS-1D model using Pydrus.
+"""
+
+import os
+import pydrus as ps
+import pandas as pd
+
+ws = "seep"
+exe = os.path.join(os.getcwd(), "hydrus")
+
+# Create the basic model
+desc = "Example 2 - Grass Field Problem (Hupselse Beek 1982)"
+
+ml = ps.Model(exe_name=exe, ws_name=ws, name="model", description=desc,
+              mass_units="-", time_unit="days", length_unit="cm")
+
+ml.time_information["tInit"] = 90
+ml.time_information["tMax"] = 273
+ml.time_information["dt"] = 0.1
+ml.time_information["dtMax"] = 1
+
+# Water inflow parameters
+ml.add_waterflow(kodbot=-1, linitw=False, free_drainage=True, ha=1e-6, hb=1e4)
+# ml.add_waterflow(kodbot=-1, linitw=False, qgwlf=True, gw_level=230,
+#                  aqh=-0.1687, bqh=-0.02674)
+
+m = pd.DataFrame(columns=["thr", "ths", "Alfa", "n", "Ks", "l"],
+                 data=[[0.0001, 0.399, 0.0174, 1.3757, 29.75, 0.5],
+                       [0.0001, 0.339, 0.0139, 1.6024, 45.34, 0.5]],
+                 index=[1, 2])
+ml.add_material(m)
+
+profile = ps.create_profile(0, -230, h=-200, dx=10)
+profile.loc[11:, ["Mat", "Lay"]] = 2
+ml.add_profile(profile)
+
+atm = pd.read_csv("data/ex2.csv", index_col=0)
+ml.add_atmosphere(atm)
+
+ml.add_observations([10, 20, 30])
+
+ml.write_files()
+rs = ml.simulate()
+
+df = ml.read_tlevel()
+df['vBot[L/T]'].plot()
---
+++ examples/example3.py
@@ -0,0 +1,49 @@
+"""
+Example script of how to setup a basic HYDRUS-1D model using Pydrus.
+"""
+
+import os
+import pydrus as ps
+import numpy as np
+import pandas as pd
+
+ws = "example2"
+exe = os.path.join(os.getcwd(), "hydrus")
+
+# Create the basic model
+desc = "Root uptake with meteorological data"
+
+ml = ps.Model(exe_name=exe, ws_name=ws, name="model", description=desc,
+              mass_units="mmol", time_unit="days", length_unit="cm")
+
+# Time info
+ml.time_information["tInit"] = 0
+ml.time_information["tMax"] = 214
+ml.time_information["dtMax"] = 1
+ml.time_information["MPL"] = 1
+
+# Add materials
+m = pd.DataFrame(columns=["thr", "ths", "Alfa", "n", "Ks", "l"], index=[1],
+                 data=[[0.095, 0.41, 0.019, 1.31, 3.4, 0.5]])
+ml.add_material(m)
+
+# Profile
+profile = ps.create_profile(bot=-100, dx=1, h=-70.5614)
+ml.add_profile(profile)
+
+# Water flow info
+ml.add_waterflow(maxit=20, tolh=1, linitw=False, kodbot=-1,
+                 seepage_face=True, hseep=-60)
+
+# Atmospheric data
+atm = pd.read_csv("data/ex3.csv")
+ml.add_atmosphere(atm)
+
+# Root uptake: does not work yet!
+# ml.add_rootwater_uptake(model=0, poptm=[-25] * m.index.size)
+
+ml.write_files()
+ml.simulate()
+
+df = ml.read_tlevel()
+df['vBot[L/T]'].plot()
