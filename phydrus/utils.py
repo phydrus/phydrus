1342b1e92b144c25811d4efd2b9633dd8f328341
Martin Vonk <vonk.mart@gmail.com>
2022-11-23 19:49:52 +0100

    remove old nodinf based functions

--- phydrus/utils.py
+++ phydrus/utils.py
@@ -4,7 +4,7 @@
 
 import logging
 from logging import handlers
-from numpy import exp, maximum, argmin, array, flip
+from numpy import exp, maximum, argmin, array, signbit, where, diff
 
 
 logger = logging.getLogger(__name__)
@@ -57,15 +57,16 @@ def _initialize_logger(logger=None, level=logging.INFO):
 
     """
     if logger is None:
-        logger = logging.getLogger('phydrus')
+        logger = logging.getLogger("phydrus")
 
     logger.setLevel(level)
     remove_file_handlers(logger)
     set_console_handler(logger)
 
 
-def set_console_handler(logger=None, level=logging.INFO,
-                        fmt="%(levelname)s: %(message)s"):
+def set_console_handler(
+    logger=None, level=logging.INFO, fmt="%(levelname)s: %(message)s"
+):
     """
     Method to add a console handler to the logger of Phydrus.
 
@@ -78,7 +79,7 @@ def set_console_handler(logger=None, level=logging.INFO,
 
     """
     if logger is None:
-        logger = logging.getLogger('phydrus')
+        logger = logging.getLogger("phydrus")
     remove_console_handler(logger)
     ch = logging.StreamHandler()
     ch.setLevel(level)
@@ -119,17 +120,23 @@ def remove_console_handler(logger=None):
 
     """
     if logger is None:
-        logger = logging.getLogger('phydrus')
+        logger = logging.getLogger("phydrus")
 
     for handler in logger.handlers:
         if isinstance(handler, logging.StreamHandler):
             logger.removeHandler(handler)
 
 
-def add_file_handlers(logger=None, filenames=('info.log', 'errors.log'),
-                      levels=(logging.INFO, logging.ERROR), maxBytes=10485760,
-                      backupCount=20, encoding='utf8', datefmt='%d %H:%M',
-                      fmt='%(asctime)s-%(name)s-%(levelname)s-%(message)s'):
+def add_file_handlers(
+    logger=None,
+    filenames=("info.log", "errors.log"),
+    levels=(logging.INFO, logging.ERROR),
+    maxBytes=10485760,
+    backupCount=20,
+    encoding="utf8",
+    datefmt="%d %H:%M",
+    fmt="%(asctime)s-%(name)s-%(levelname)s-%(message)s",
+):
     """
     Method to add file handlers in the logger of Phydrus.
 
@@ -142,15 +149,15 @@ def add_file_handlers(logger=None, filenames=('info.log', 'errors.log'),
 
     """
     if logger is None:
-        logger = logging.getLogger('phydrus')
+        logger = logging.getLogger("phydrus")
     # create formatter
     formatter = logging.Formatter(fmt=fmt, datefmt=datefmt)
 
     # create file handlers, set the level & formatter, and add it to the logger
     for filename, level in zip(filenames, levels):
-        fh = handlers.RotatingFileHandler(filename, maxBytes=maxBytes,
-                                          backupCount=backupCount,
-                                          encoding=encoding)
+        fh = handlers.RotatingFileHandler(
+            filename, maxBytes=maxBytes, backupCount=backupCount, encoding=encoding
+        )
         fh.setLevel(level)
         fh.setFormatter(formatter)
         logger.addHandler(fh)
@@ -168,7 +175,7 @@ def remove_file_handlers(logger=None):
         and packages.
     """
     if logger is None:
-        logger = logging.getLogger('phydrus')
+        logger = logging.getLogger("phydrus")
     for handler in logger.handlers:
         if isinstance(handler, handlers.RotatingFileHandler):
             logger.removeHandler(handler)
@@ -176,7 +183,7 @@ def remove_file_handlers(logger=None):
 
 def partitioning_grass(P, ET, a=0.45, ch=5, k=0.463, return_SCF=False):
     """
-    Partitioning according to equation 2.75 in the Manual v4.0 and 
+    Partitioning according to equation 2.75 in the Manual v4.0 and
     Sutanto, Wenninger, Coenders and Uhlenbrook [2021]
 
     Parameters
@@ -211,58 +218,3 @@ def partitioning_grass(P, ET, a=0.45, ch=5, k=0.463, return_SCF=False):
         return Pnet, I, Etp, Esp, SCF
     else:
         return Pnet, I, Etp, Esp
-
-
-def get_recharge(nodinf, recharge_depth):
-    """ Function to obtain the flux at a certain depth 
-    from the NOD_INF.OUT file.
-
-    Parameters
-    ----------
-    nodinf (dictionary) - Dictionary of the NOD_INF.OUT file 
-    with the timesteps as keys. The data in the dictionary 
-    contains a DataFrame for each timestep with the column 
-    'Depth' and 'Flux'. Take a look at read.read_nodinf()
-    
-    recharge depth (float) - Float to indicate depth from which
-    the recharge can be extracted.
-
-    Standard Output
-    ----------
-    recharge (array) - Numpy array with the recharge at a defined depth.
-
-    """    
-    idx = argmin(abs(nodinf[0]['Depth'].values - recharge_depth))
-    recharge = []
-    for i in list(nodinf.keys()):
-        recharge.append(nodinf[i]['Flux'].iloc[idx])
-    return array(recharge)
-
-
-def get_gwt(nodinf):
-    """ Extract the location of the groundwater table from the NOD_INF.OUT file. 
-    The location of the groundwater table is taken where the pressure head = 0 (bottom up approach)
-
-    Parameters
-    ----------
-    nodinf (dictionary) - Dictionary of the NOD_INF.OUT file with the timesteps as keys. The data in the dictionary contains a DataFrame for each timestep with the column 'Depth' and 'Head'.
-
-    Standard Output
-    ----------
-    groundwater table (array) - numpy array with the location of the groundwater table.
-    """
-    gwl = []
-    for i in list(nodinf.keys()):
-        j = 0
-        while flip(nodinf[i]['Head'].values)[j] > 0 and j < len(nodinf[i]) - 1:
-            j += 1
-        idx = len(nodinf[0]['Head']) - j
-        if idx == 0:
-            gwl_new = nodinf[i]['Head'][idx]
-        elif idx == len(nodinf[i]['Head']):
-            gwl_new = nodinf[i]['Depth'].iloc[-1]
-        else:
-            gwl_new = (0 - nodinf[i]['Head'].iloc[idx - 1]) * (nodinf[i]['Depth'].iloc[idx] - nodinf[i]['Depth'].iloc[idx - 1]) / (
-                nodinf[i]['Head'].iloc[idx] - nodinf[i]['Head'].iloc[idx - 1]) + nodinf[i]['Depth'].iloc[idx - 1]
-        gwl.append(gwl_new)
-    return array(gwl)
